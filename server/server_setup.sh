5c7ecc87f514d2f2ab5e372c3c81792a32828867
wagerc97 <wager.clemens@gmail.com>
2025-08-29 23:56:37 +0200

    server setup streamlined

--- server/server_setup.sh
+++ server/server_setup.sh
@@ -1,15 +1,18 @@
-# Setup the environment 
+# Setup script for server environment
+
 export MAMBA_EXE='/home/wager/micromamba/bin/micromamba';
 export MAMBA_ROOT_PREFIX='/home/wager/micromamba';
 eval "$(/home/wager/micromamba/bin/micromamba shell hook --shell bash)"
 micromamba --version
-micromamba env list
-echo "--> using Python: $(which python)"
 
-micromamba create -f vllm_server_env.yaml -y
+micromamba deactivate
+micromamba create -n vllm_aug2025 python=3.12 -y
 micromamba activate vllm_aug2025
+micromamba env list
+#export VLLM_USE_V1=0
 
-export VLLM_USE_V1=0
+micromamba install -c conda-forge libstdcxx-ng -y
+pip install uv
 
 uv pip install --pre vllm==0.10.1+gptoss \
     --extra-index-url https://wheels.vllm.ai/gpt-oss/ \
@@ -17,5 +20,3 @@ uv pip install --pre vllm==0.10.1+gptoss \
     --index-strategy unsafe-best-match
 
 uv pip install openai --upgrade
-
-vllm serve openai/gpt-oss-20b
