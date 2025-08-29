0a02f32d110b13444bb70746fce319042bd6a197
wagerc97 <wager.clemens@gmail.com>
2025-08-29 20:25:41 +0200

    change port 8000 -> 8010

--- vllm.sh
+++ vllm.sh
@@ -1,7 +1,7 @@
 python -u \
 	-m vllm.entrypoints.openai.api_server \
 	--host 0.0.0.0 \
-	--port 8000 \
+	--port 8010 \
 	--model openai/gpt-oss-20b \
 	--served-model-name code-gen \
 	--tensor-parallel-size 8
