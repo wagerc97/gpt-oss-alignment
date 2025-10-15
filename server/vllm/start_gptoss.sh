94032dd59308e0a5ed94a58d097b484bf9579ccb
wagerc97 <wager.clemens@gmail.com>
2025-10-15 10:23:59 +0200

    update paths to user $USER

--- server/vllm/start_gptoss.sh
+++ server/vllm/start_gptoss.sh
@@ -2,9 +2,9 @@
 # Start the vLLM server for GPT-OSS-20B model
 
 # Load micromamba
-export MAMBA_EXE='/home/wager/micromamba/bin/micromamba';
-export MAMBA_ROOT_PREFIX='/home/wager/micromamba';
-eval "$(/home/wager/micromamba/bin/micromamba shell hook --shell bash)"
+export MAMBA_EXE='/home/$USER/micromamba/bin/micromamba';
+export MAMBA_ROOT_PREFIX='/home/$USER/micromamba';
+eval "$(/home/$USER/micromamba/bin/micromamba shell hook --shell bash)"
 micromamba --version
 micromamba activate vllm_oct25
 micromamba env list
